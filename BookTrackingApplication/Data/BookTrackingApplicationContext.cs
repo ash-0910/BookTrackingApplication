c19c1ae03226bb65f407fa56da0db2d89f0556be
ash-0910 <A00244471@mycambrian.ca>
2021-12-15 23:30:56 -0500

    added models

---
+++ BookTrackingApplication/Data/BookTrackingApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookTrackingApplication.Models;
+
+namespace BookTrackingApplication.Data
+{
+    public class BookTrackingApplicationContext : DbContext
+    {
+        public BookTrackingApplicationContext (DbContextOptions<BookTrackingApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookTrackingApplication.Models.Book> Book { get; set; }
+    }
+}
---
+++ BookTrackingApplication/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTrackingApplication.Models
+{
+    public class Book
+    {
+        [Key]
+        public string ISBN { get; set; }
+
+        public string Title { get; set; }
+
+        public int Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
